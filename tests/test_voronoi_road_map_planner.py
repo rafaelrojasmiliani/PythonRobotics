e3f09f825b1564c8c84c74aa62156bad57b17285
Rojas Rafael <rafael.rojas@unibz.it>
2020-08-25 09:32:58 +0200

    voronoi test updated

--- tests/test_voronoi_road_map_planner.py
+++ tests/test_voronoi_road_map_planner.py
@@ -2,16 +2,21 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__))
-                + "/../PathPlanning/VisibilityRoadMap/")
+                + "/..")
 
-from unittest import TestCase
+import unittest 
 from PathPlanning.VisibilityRoadMap import visibility_road_map as m
 
 print(__file__)
 
 
-class Test(TestCase):
+class Test(unittest.TestCase):
 
     def test1(self):
-        m.show_animation = False
+        m.show_animation = True
         m.main()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
